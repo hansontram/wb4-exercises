f9b41e0b684727753f347cca75b45692ca6de718
Hanson Tram <htram.dev@gmail.com>
2023-10-31 13:03:43 -0700

    finished second items

--- product_code.js
+++ product_code.js
@@ -1,3 +1,26 @@
-function parsePartCode (){
-    
+function parsePartCode(partCode) {
+  let colonIndex = partCode.indexOf(":");
+  let dashIndex = partCode.indexOf("-");
+
+  let supplierCode = partCode.substring(0, colonIndex);
+  let productNumber = partCode.substring(colonIndex + 1, dashIndex);
+  let size = partCode.substring(dashIndex + 1);
+
+  return {
+    supplierCode,
+    productNumber,
+    size,
+  };
 }
+
+// parsePartCode("XYZ:1234-L")
+let partCode1 = "XYZ:1234-L";
+let part1 = parsePartCode(partCode1);
+console.log(
+  "Supplier: " +
+    part1.supplierCode +
+    " Product Number: " +
+    part1.productNumber +
+    " Size: " +
+    part1.size
+);
